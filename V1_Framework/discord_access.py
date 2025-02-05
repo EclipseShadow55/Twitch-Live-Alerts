f604403765371c57dd0fd545c81df1aa1ab5bcb7
EShadow5 <141190316+EShadow5@users.noreply.github.com>
2025-02-04 22:03:25 -0800

    Commit

--- V1_Framework/discord_access.py
+++ V1_Framework/discord_access.py
@@ -10,9 +10,9 @@ def send_message(webhook_url, message):
     return [message, [response.status_code == 204, response.status_code, response.text]]
 
 
-def live_alerts(webhook_url, channel_name, info):
+def live_alerts(webhook_url, channel_name, info, test=False):
     with open("info.json", "r") as f:
-        additional_url = json.load(f)["additional_urls"]
+        additional_urls = json.load(f)["additional_urls"]
 
     if info is not None:
         message = "\n".join([f"{info['user_name']} Is Now Live!",
@@ -24,11 +24,11 @@ def live_alerts(webhook_url, channel_name, info):
     else:
         message = f"{channel_name} Is Offline!\n<https://www.twitch.tv/{channel_name}>"
 
-    if webhook_url is not None:
+    if webhook_url is not None and not test:
         if isinstance(webhook_url, list):
-            webhook_url.append(additional_url)
-            return [send_message(url, message) for url in webhook_url]
+            webhook_url.append(additional_urls)
+            return [send_message(url, message) for url in webhook_url] + [send_message(url, message) for url in additional_urls]
         else:
-            return [send_message(webhook_url, message), send_message(additional_url, message)]
+            return [send_message(url, message) for url in additional_urls].append(send_message(webhook_url, message))
     else:
-        return send_message(additional_url, message)
+        return [send_message(url, message) for url in additional_urls]
