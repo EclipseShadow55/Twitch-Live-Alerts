d3a98115e79312edcc97d8cc765455c56faa51fd
EShadow5 <141190316+EShadow5@users.noreply.github.com>
2025-02-07 17:06:13 -0800

    Commit

--- Live-Alerts-Python/discord_access.py
+++ Live-Alerts-Python/discord_access.py
@@ -13,7 +13,7 @@ def send_message(webhook_url, message, embeds):
 
 def live_alerts(webhook_urls, channel_name, info, test=False):
     with open("Data/info.json", "r") as f:
-        additional_urls = json.load(f)["additional_urls"]
+        additional_urls = json.load(f).get("additional_urls", [])
     if info is not None:
         message = f"{info['user_name']} Is Now Live!"
         embeds = [
--- Live-Alerts-Python/tester.py
+++ Live-Alerts-Python/tester.py
@@ -7,7 +7,7 @@ def main():
     off = "NyxLitten"
     on = "meerkatmiki"
 
-    with open("../../Required/data.json", "r") as f:
+    with open("../Required/data.json", "r") as f:
         data = json.load(f)
     info = ta.get_stream_data([on], data[0], ta.get_access_token(data[0], data[1]))[0]
     print(da.live_alerts(None, on, info))
