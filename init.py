e54c36c7a2c91154bebdf8c59b5e70205cfab296
EShadow5 <141190316+EShadow5@users.noreply.github.com>
2025-03-29 20:04:57 -0700

    Commit
    Signed-off-by: EShadow5 <141190316+EShadow5@users.noreply.github.com>

--- init.py
+++ init.py
@@ -19,10 +19,6 @@ import requests
 import os
 import base64
 
-def get_subdirs(directory):
-    subdirs = [d for d in os.listdir(directory) if os.path.isdir(os.path.join(directory, d))]
-    return subdirs
-
 def get_github_file_content(owner, repo, path, branch='main', token=None):
     url = f"https://api.github.com/repos/{owner}/{repo}/contents/{path}?ref={branch}"
     headers = {}
@@ -37,79 +33,79 @@ def get_github_file_content(owner, repo, path, branch='main', token=None):
 
 if __name__ == "__main__":
     print("Initializing required directories...")
-    if not os.path.isdir("Logs"):
-        os.mkdir("Logs")
+    if not os.path.isdir("../Logs"):
+        os.mkdir("../Logs")
         print("Logs directory created.")
-    if not os.path.isfile("Logs/log.txt"):
-        with open("Logs/log.txt", "x") as f:
+    if not os.path.isfile("../Logs/log.txt"):
+        with open("../Logs/log.txt", "x", encoding="utf-8") as f:
             f.write("")
         print("Log file created.")
-    if not os.path.isfile("Logs/errors.txt"):
-        with open("Logs/errors.txt", "x") as f:
+    if not os.path.isfile("../Logs/errors.txt"):
+        with open("../Logs/errors.txt", "x", encoding="utf-8") as f:
             f.write("")
         print("Error log file created.")
-    if not os.path.isdir("Data"):
-        os.mkdir("Data")
+    if not os.path.isdir("../Data"):
+        os.mkdir("../Data")
         print("Data directory created.")
-    if not os.path.isfile("Data/channels.json"):
-        with open("Data/channels.json", "x") as f:
+    if not os.path.isfile("../Data/channels.json"):
+        with open("../Data/channels.json", "x", encoding="utf-8") as f:
             json.dump({"CHANNEL-1": None, "CHANNEL-2": "URL-1", "CHANNEL-3": ["URL-1", "URL-2", "URL-3"]}, f, indent=4)
         print("Channel data file created.")
-    if not os.path.isfile("Data/info.json"):
-        with open("Data/info.json", "x") as f:
+    if not os.path.isfile("../Data/info.json"):
+        with open("../Data/info.json", "x", encoding="utf-8") as f:
             json.dump({"additional-urls": []}, f, indent=4)
         print("Info data file created.")
-    if not os.path.isdir("Required"):
-        os.mkdir("Required")
+    if not os.path.isdir("../Required"):
+        os.mkdir("../Required")
         print("Required directory created.")
-    if not os.path.isfile("Required/auth.json"):
-        with open("Required/auth.json", "x") as f:
+    if not os.path.isfile("../Required/auth.json"):
+        with open("../Required/auth.json", "x", encoding="utf-8") as f:
             json.dump(["YOUR-CLIENT-ID", "YOUR-CLIENT-SECRET"], f, indent=4)
         print("Authorization data file created.")
     print("Required initialization complete.")
     print("Initializing legal files...")
-    if not os.path.isfile("LICENSE"):
-        with open("LICENSE", "x") as f:
-            f.write(get_github_file_content("EclipseShadow55", "Twitch-Live-Alerts", "LICENSE"))
+    if not os.path.isfile("../LICENSE"):
+        with open("../LICENSE", "x", encoding="utf-8") as f:
+            f.write(get_github_file_content("EclipseShadow55", "Twitch-Live-Alerts", "../LICENSE"))
         print("LICENSE file created.")
-    if not os.path.isfile("NOTICE"):
-        with open("NOTICE", "x") as f:
-            f.write(get_github_file_content("EclipseShadow55", "Twitch-Live-Alerts", "NOTICE"))
+    if not os.path.isfile("../NOTICE"):
+        with open("../NOTICE", "x", encoding="utf-8") as f:
+            f.write(get_github_file_content("EclipseShadow55", "Twitch-Live-Alerts", "../NOTICE"))
         print("NOTICE file created.")
-    if not os.path.isfile("README.md"):
-        with open("README.md", "x") as f:
-            f.write(get_github_file_content("EclipseShadow55", "Twitch-Live-Alerts", "README.md"))
+    if not os.path.isfile("../README.md"):
+        with open("../README.md", "x", encoding="utf-8") as f:
+            f.write(get_github_file_content("EclipseShadow55", "Twitch-Live-Alerts", "../README.md"))
         print("README file created.")
-    if not os.path.isdir("Legal"):
-        os.mkdir("Legal")
-    if not os.path.isdir("Legal/Requests"):
-        os.mkdir("Legal/Requests")
-    if not os.path.isfile("Legal/Requests/LICENSE"):
-        with open("Legal/Requests/LICENSE", "x") as f:
-            f.write(get_github_file_content("EclipseShadow55", "Twitch-Live-Alerts", "Legal/Requests/LICENSE"))
+    if not os.path.isdir("../Legal"):
+        os.mkdir("../Legal")
+    if not os.path.isdir("../Legal/Requests"):
+        os.mkdir("../Legal/Requests")
+    if not os.path.isfile("../Legal/Requests/LICENSE"):
+        with open("../Legal/Requests/LICENSE", "x", encoding="utf-8") as f:
+            f.write(get_github_file_content("EclipseShadow55", "Twitch-Live-Alerts", "../Legal/Requests/LICENSE"))
         print("Requests license file created.")
-    if not os.path.isdir("Legal/Winotify"):
-        os.mkdir("Legal/Winotify")
-    if not os.path.isfile("Legal/Winotify/LICENSE"):
-        with open("Legal/Winotify/LICENSE", "x") as f:
-            f.write(get_github_file_content("EclipseShadow55", "Twitch-Live-Alerts", "Legal/Winotify/LICENSE"))
-    if not os.path.isfile("Legal/Winotify/CREDITS.md"):
-        with open("Legal/Winotify/CREDITS.md", "x") as f:
-            f.write(get_github_file_content("EclipseShadow55", "Twitch-Live-Alerts", "Legal/Winotify/CREDITS.md"))
-    if not os.path.isfile("Legal/Requests/NOTICE"):
-        with open("Legal/Requests/NOTICE", "x") as f:
-            f.write(get_github_file_content("EclipseShadow55", "Twitch-Live-Alerts", "Legal/Requests/NOTICE"))
-    if not os.path.isfile("Legal/Reqeusts/CREDITS.rst"):
-        with open("Legal/Requests/CREDITS.rst", "x") as f:
-            f.write(get_github_file_content("EclipseShadow55", "Twitch-Live-Alerts", "Legal/Requests/CREDITS.rst"))
+    if not os.path.isdir("../Legal/Winotify"):
+        os.mkdir("../Legal/Winotify")
+    if not os.path.isfile("../Legal/Winotify/LICENSE"):
+        with open("../Legal/Winotify/LICENSE", "x", encoding="utf-8") as f:
+            f.write(get_github_file_content("EclipseShadow55", "Twitch-Live-Alerts", "../Legal/Winotify/LICENSE"))
+    if not os.path.isfile("../Legal/Winotify/CREDITS.md"):
+        with open("../Legal/Winotify/CREDITS.md", "x", encoding="utf-8") as f:
+            f.write(get_github_file_content("EclipseShadow55", "Twitch-Live-Alerts", "../Legal/Winotify/CREDITS.md"))
+    if not os.path.isfile("../Legal/Requests/NOTICE"):
+        with open("../Legal/Requests/NOTICE", "x", encoding="utf-8") as f:
+            f.write(get_github_file_content("EclipseShadow55", "Twitch-Live-Alerts", "../Legal/Requests/NOTICE"))
+    if not os.path.isfile("../Legal/Requests/CREDITS.rst"):
+        with open("../Legal/Requests/CREDITS.rst", "x", encoding="utf-8") as f:
+            f.write(get_github_file_content("EclipseShadow55", "Twitch-Live-Alerts", "../Legal/Requests/CREDITS.rst"))
     print("Legal files initialized.")
     if input("Would you like to see the setup instructions now? (y/n): ").lower()[0] == "y":
-        if os.path.isfile("instructions.py"):
+        if os.path.isfile("../instructions.py"):
             os.chdir(os.path.dirname(os.path.abspath(__file__)))
             os.system("python instructions.py")
-        elif os.path.isfile("instructions.exe"):
+        elif os.path.isfile("../instructions.exe"):
             os.chdir(os.path.dirname(os.path.abspath(__file__)))
-            os.system("instructions.exe")
+            os.system("../instructions.exe")
         else:
             print("Instructions not found.")
             print("You can find the instructions in the README.md file.")
--- instructions.py
+++ instructions.py
@@ -1,10 +1,13 @@
 import os
 
+step7_3 = "Step 7.3: Then type 'python twitch_live_alerts.py' followed by any of the following arguments:" if not os.path.isfile("twitch_live_alerts.exe") else "Step 7.3: Then type 'twitch_live_alerts.exe' followed by any of the following flags:"
+step7 = "Step 7: Run twitch_live_alerts.py to start monitoring the channels." if not os.path.isfile("twitch_live_alerts.exe") else "Step 7: Run twitch_live_alerts.exe to start monitoring the channels."
+step3 = "Step 3: Run init.py to initialize the required directories and files if you haven't already." if not os.path.isfile("init.exe") else "Step 3: Run init.exe to initialize the required directories and files if you haven't already"
 stuff = [
     "Step 0: Download the latest version of the program from the GitHub repository.",
     ["Step 1: Create a new directory for the program."],
     ["Step 2: Move all files to the new directory."],
-    ["Step 3: Run init.py to initialize the required directories and files if you haven't already." if __file__[__file__.rfind("."):] == ".py" else "Step 3: Run init.exe to initialize the required directories and files if you haven't already"],
+    [step3],
     ["Step 4: Open the 'channels.json' file that is in the 'Data' directory in a text editor, and fill out all the channels you want to monitor in these formats:",
      "\t\"CHANNEL-NAME\": null",
      "\t\"CHANNEL-NAME\": \"URL\"",
@@ -13,34 +16,35 @@ stuff = [
      "And for the url, use a webhook from a discord channel (you can follow this tutorial to get the webhook url: https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks"],
     ["Step 5: Open the 'auth.json' file in the 'Required' directory in a text editor, and fill out the required authorization data in this format:",
      "\t[\"TWITCH-CLIENT-ID\", \"TWITCH-CLIENT-SECRET\"]"],
-    ["\tStep 5.1: To get the client id and client secret, you need to first go to https://dev.twitch.tv"],
-    ["\tStep 5.2: Then you need to log in with twitch, then click on 'Your Console' in the top right"],
-    ["\tStep 5.3: Then click on 'Register Your Application'"],
-    ["\tStep 5.4: Start with filling out the name. You can call it whatever you want, but I recommend calling it Twitch-Live-Alerts"],
-    ["\tStep 5.5: Then fill out the OAuth Redirect URL. Use 'http://localhost' for this, since we won't be using this feature"],
-    ["\tStep 5.6: Then fill out the Category. I recommend using 'Other' for this"],
-    ["\tStep 5.7: Then click 'I'm not a robot' and then click 'Create'"],
-    ["\tStep 5.8: Then you will be taken to the 'Manage Your Applications' page"],
-    ["\tStep 5.9: Click the manage button on the application you just created"],
-    ["\tStep 5.10: You will see a Client ID. Copy this and paste it into the first spot in the 'auth.json' file"],
-    ["\tStep 5.11: Then click 'New Secret' to generate a client secret, and click 'Ok' in the pop up that appears"],
-    ["\tStep 5.12: Then you will see a Client Secret. Copy this and paste it into the second spot in the 'auth.json' file",
-     "\t\tMake sure to keep this secret, as it is used to authenticate the program with twitch"],
-    ["\tStep 5.13: Then click 'Save' at the bottom of the page"],
-    ["\tStep 5.14: And you are done with the authorization data!"],
+    ["Step 5.1: To get the client id and client secret, you need to first go to https://dev.twitch.tv"],
+    ["Step 5.2: Then you need to log in with twitch, then click on 'Your Console' in the top right"],
+    ["Step 5.3: Then click on 'Register Your Application'"],
+    ["Step 5.4: Start with filling out the name. You can call it whatever you want, but I recommend calling it Twitch-Live-Alerts"],
+    ["Step 5.5: Then fill out the OAuth Redirect URL. Use 'http://localhost' for this, since we won't be using this feature"],
+    ["Step 5.6: Then fill out the Category. I recommend using 'Other' for this"],
+    ["Step 5.7: Then click 'I'm not a robot' and then click 'Create'"],
+    ["Step 5.8: Then you will be taken to the 'Manage Your Applications' page"],
+    ["Step 5.9: Click the manage button on the application you just created"],
+    ["Step 5.10: You will see a Client ID. Copy this and paste it into the first spot in the 'auth.json' file"],
+    ["Step 5.11: Then click 'New Secret' to generate a client secret, and click 'Ok' in the pop up that appears"],
+    ["Step 5.12: Then you will see a Client Secret. Copy this and paste it into the second spot in the 'auth.json' file",
+     "\tMake sure to keep this secret, as it is used to authenticate the program with twitch"],
+    ["Step 5.13: Then click 'Save' at the bottom of the page"],
+    ["Step 5.14: And you are done with the authorization data!"],
     ["Step 6: Open the 'info.json' file that is in the 'Data' directory in a text editor, and fill out any additional URLs you want to post to alerts to in this format:",
      "\t{\"additional-urls\": [\"URL1\", \"URL2\"]}",
      "These are just more discord webhooks that you want to post alerts to, but these ones will post alerts for every streamer on the list"],
-    ["Step 7: Run main.pyw to start monitoring the channels.",
+    [step7,
      "If you want to run the program with special settings, you can do the following:"],
-    ["\tStep 7.1: Open the command prompt by hitting the windows key and typing 'cmd' into the text box"],
-    ["\tStep 7.2: Then navigate to the directory where the program is located by typing 'cd ' followed by the path to the directory"],
-    ["\tStep 7.3: Then type 'python main.pyw' followed by any of the following arguments:" if __file__[__file__.rfind("."):] == ".py" else "\tStep 7.3: Then type 'main.exe' followed by any of the following flags:",
-     "\t\t'--no-notif': This will disable Windows notifications for when a streamer goes live or offline, so only the discord channels will get the alert",
-     "\t\t'--no-offline': This will disable alerts for when a streamer goes offline",
-     "\t\t'--start-ignore': This will stop the program from posting an alert for any streamer that is live when the program is started, so only streamers that go live after the program is started will have an alert posted about them"],
+    ["Step 7.1: Open the command prompt by hitting the windows key and typing 'cmd' and pressing enter"],
+    ["Step 7.2: Then navigate to the directory where the program is located by typing 'cd ' followed by the path to the directory",
+     "\tYou can find this by clicking on the address bar in the file explorer while you in the directory, and copying the path"],
+    [step7_3,
+     "\t'--no-notif': This will disable Windows notifications for when a streamer goes live or offline, so only the discord channels will get the alert",
+     "\t'--no-offline': This will disable alerts for when a streamer goes offline",
+     "\t'--start-ignore': This will stop the program from posting an alert for any streamer that is live when the program is started, so only streamers that go live after the program is started will have an alert posted about them"],
     ["And you are done! The program will now monitor the channels you have set up and post alerts to the discord channels you have set up.",
-     "Note: It is vital that you keep all the files in the given file structure (so both 'channels.json' and 'info.json' in a director called 'Data'), the program will not function otherwise."]
+     "Note: It is vital that you keep all the files in the given file structure (so both 'channels.json' and 'info.json' in a directory called 'Data', etc.), the program will not function otherwise."]
 ]
 
 if __name__ == "__main__":
@@ -48,7 +52,7 @@ if __name__ == "__main__":
     while i < len(stuff):
         os.system("cls")
         if i == 0:
-            if not os.path.isfile("main.pyw") and not os.path.isfile("main.exe"):
+            if not os.path.isfile("Distros/twitch_live_alerts.py") and not os.path.isfile("twitch_live_alerts.exe"):
                 print(stuff[0])
                 inp = input(f"Press Enter to continue, 'back' to go back to the last step, or 'exit' to exit the instructions: ")
             else:
@@ -58,7 +62,7 @@ if __name__ == "__main__":
                 print(j)
             inp = input(f"Press Enter to continue, 'back' to go back to the last step, or 'exit' to exit the instructions: ")
         if inp.lower() == "exit":
-            exit(0)
+            break
         elif inp.lower() == "back":
             i -= 1
         else:
